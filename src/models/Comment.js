dbc153ff98275d058a303861990b559db8686a6c
kasra <kasramajdy81@gmail.com>
2025-07-29 23:05:37 +0330

    Feat: add User model schema with role and token fields

---
+++ src/models/Comment.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose")
+
+const commentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+        required: true
+    },
+    parentComment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+        default: null
+    },
+    isApproved: {
+        type: Boolean,
+        default: false
+    },
+    isAnswered: {
+        type: Boolean,
+        default: false
+    },
+    answerText: {
+        type: String,
+        default: null
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const CommentModel = mongoose.model("Comment", commentSchema)
+
+module.exports = CommentModel
