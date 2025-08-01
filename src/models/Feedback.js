56e9e0105051fdbd7fe09cd2fb07994b3b530bf9
kasra <kasramajdy81@gmail.com>
2025-08-01 18:21:44 +0330

    Feat: extend Feedback model with user contact fields (firstName, lastName, email, number)

--- src/models/Feedback.js
+++ src/models/Feedback.js
@@ -5,6 +5,22 @@ const feedbackSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    number:{
+        type: String,
+        required: true
+    },
     type: {
         type: String,
         enum: ["question", "criticism", "suggestion"],
